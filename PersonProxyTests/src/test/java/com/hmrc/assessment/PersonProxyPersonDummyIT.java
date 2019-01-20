6159e68cfda451674dcb3c592514bbed166d17cb
Guido <guidev83@gmail.com>
2019-01-20 19:59:58 +0000

    Fucking externalised fucking properties for the test (no Spring)

--- PersonProxyTests/src/test/java/com/hmrc/assessment/PersonProxyPersonDummyIT.java
+++ PersonProxyTests/src/test/java/com/hmrc/assessment/PersonProxyPersonDummyIT.java
@@ -1,6 +1,9 @@
 package com.hmrc.assessment;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static io.restassured.RestAssured.given;
 
@@ -8,18 +11,26 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class PersonProxyPersonDummyIT {
 
+    private static final Logger LOG = LoggerFactory.getLogger(PersonProxyPersonDummyIT.class);
+
+    private static final String PERSON_DUMMY_API_URL_PROPERTY = "com.hmrc.assessment.dummypersonapi.url";
+
     @Test
     public void shouldCallPersonDummyAPI() throws Exception {
 
+        final String personDummyAPIUrl = System.getProperty(PERSON_DUMMY_API_URL_PROPERTY);
+
+        LOG.info("personDummyAPIUrl: {}", personDummyAPIUrl);
+
         given()
-                .log().all()
                 .header("Content-Type", "application/json")
                 .header("Accept", "application/json")
                 .body("{ \"name\" : \"Guido\" }")
         .when()
                 .log().all()
-                .post("http://172.16.0.64:8280/persondummy ")
+                .post(personDummyAPIUrl)
         .then()
+                .log().all()
                 .statusCode(200)
                 .body("message", equalTo("Hello Guido!"));
 
