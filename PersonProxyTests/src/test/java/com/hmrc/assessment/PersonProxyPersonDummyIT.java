18ad39971350d7f950851b8a8c066159ae87fbb8
Guido <guidev83@gmail.com>
2019-01-21 00:05:58 +0000

    Positive/negative tests for the schema validation

--- PersonProxyTests/src/test/java/com/hmrc/assessment/PersonProxyPersonDummyIT.java
+++ PersonProxyTests/src/test/java/com/hmrc/assessment/PersonProxyPersonDummyIT.java
@@ -9,10 +9,20 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+import static java.net.HttpURLConnection.*;
 
 import static io.restassured.RestAssured.given;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class PersonProxyPersonDummyIT {
@@ -21,7 +31,10 @@ public class PersonProxyPersonDummyIT {
 
     private static final int WIREMOCK_PORT = 42569;
 
+    private static final String WIREMOCK_URL_PATH = "/external/person";
+
     private static final String PERSON_DUMMY_API_URL_PROPERTY = "com.hmrc.assessment.dummypersonapi.url";
+    private static final String PERSONPROXY_SERVICE__URL_PROPERTY = "com.hmrc.assessment.personproxyservice.url";
 
     @BeforeAll
     public static void connectToWiremock() {
@@ -46,11 +59,9 @@ public class PersonProxyPersonDummyIT {
                 .header("Accept", "application/json")
                 .body("{ \"name\" : \"Guido\" }")
         .when()
-                .log().all()
                 .post(personDummyAPIUrl)
         .then()
-                .log().all()
-                .statusCode(200)
+                .statusCode(HTTP_OK)
                 .body("message", equalTo("Hello Guido!"));
 
     }
@@ -58,7 +69,7 @@ public class PersonProxyPersonDummyIT {
     @Test
     public void shouldSetupWireMock() {
 
-        stubFor(post(urlEqualTo("/external/person"))
+        stubFor(post(urlEqualTo(WIREMOCK_URL_PATH))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/xml")
                         .withBody("<?xml version=\"1.0\" encoding=\"UTF-8\"?><result>all good</result>")));
@@ -66,33 +77,75 @@ public class PersonProxyPersonDummyIT {
         given()
                 .header("Accept", "application/xml")
         .when()
-                .log().all()
-                .post("http://localhost:" + WIREMOCK_PORT + "/external/person")
+                .post("http://localhost:" + WIREMOCK_PORT + WIREMOCK_URL_PATH)
         .then()
-                .log().all()
-                .statusCode(200)
+                .statusCode(HTTP_OK)
                 .body("result", equalTo("all good"));
 
     }
 
     @Test
-    public void shouldTestProxyService() {
+    public void shouldTestProxyService() throws URISyntaxException, IOException {
 
-        stubFor(post(urlEqualTo("/external/person"))
+        final String personProxyUrl = System.getProperty(PERSONPROXY_SERVICE__URL_PROPERTY);
+
+        stubFor(post(urlEqualTo(WIREMOCK_URL_PATH))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/xml")
                         .withBody("<?xml version=\"1.0\" encoding=\"UTF-8\"?><result>all good</result>")));
 
+        String request = new String(
+                Files.readAllBytes( Paths.get(getClass().getResource("/person-request.xml").toURI()) ),
+                StandardCharsets.UTF_8
+        );
+
         given()
                 .header("Accept", "application/xml")
+                .header("Content-Type", "application/xml")
+                .body(request)
         .when()
-                .log().all()
-                .post("http://guido-desktop:8280/services/PersonProxy")
+                .post(personProxyUrl)
         .then()
-                .log().all()
-                .statusCode(200)
+                .statusCode(HTTP_OK)
                 .body("result", equalTo("all good"));
-        
+
+        verify(
+                exactly(1),
+                postRequestedFor(urlEqualTo(WIREMOCK_URL_PATH))
+                        .withRequestBody(containing("<postcode>BS15 87L</postcode>"))
+        );
+
     }
 
+    @Test
+    public void shouldTestProxyServiceWithBadRequest() throws URISyntaxException, IOException {
+
+        final String personProxyUrl = System.getProperty(PERSONPROXY_SERVICE__URL_PROPERTY);
+
+        stubFor(post(urlEqualTo(WIREMOCK_URL_PATH))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/xml")
+                        .withBody("<?xml version=\"1.0\" encoding=\"UTF-8\"?><result>all good</result>")));
+
+        String request = new String(
+                Files.readAllBytes( Paths.get(getClass().getResource("/person-request-bad.xml").toURI()) ),
+                StandardCharsets.UTF_8
+        );
+
+        given()
+                .header("Accept", "application/xml")
+                .header("Content-Type", "application/xml")
+                .body(request)
+        .when()
+                .post(personProxyUrl)
+        .then()
+                .statusCode(HTTP_BAD_REQUEST)
+                .body(containsString("Invalid request"));
+
+        verify(
+                exactly(0),
+                postRequestedFor(urlEqualTo(WIREMOCK_URL_PATH))
+        );
+
+    }
 }
